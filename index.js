f1b07d78fa9684205f050f2e68ba964dba16e9a4
TapsHTS <tapsmc12@gmail.com>
2022-09-28 13:47:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const baseUrl = 'https://alertzy.app/send'
+
+async function request(accountKey, title, message) {
+    const url = baseUrl + '?accountKey=' + accountKey + '?title=' + title + '&message=' + encodeURIComponent(message);
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const json = await response.json();
+    return json;
+}
+
+module.exports = {
+    sendCustomAlert: async function (accountKey, title, message) {
+        return await request(accountKey, title, message);
+    },
+}
